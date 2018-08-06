25367a2354407cb1e68f8242a302673c5980a9dd
sunnyhyo <sunnyhyo77@gmail.com>
2018-08-06 18:10:48 +0900

    set;

---
+++ bns_setting.R
@@ -0,0 +1,17 @@
+setwd("C:/Users/HS/Documents/GitHub/champion_data")
+#train data
+activity <- read.csv("./train/train_activity.csv")
+guild <- read.csv("./train/train_guild.csv")
+label <- read.csv("./train/train_label.csv")
+party <- read.csv("./train/train_label.csv")
+payment <- read.csv("./train/train_payment.csv")
+trade <- read.csv("./train/train_trade.csv")
+#test data
+tst_activity <- read.csv("./test/test_activity.csv")
+tst_guild <- read.csv("./test/test_guild.csv")
+tst_label <- read.csv("./test/test_label.csv")
+tst_party <- read.csv("./test/test_party.csv")
+tst_payment <- read.csv("./test/test_payment.csv")
+tst_trade <- read.csv("./test/test_trade.csv")
+
+#EDA
