05281cfe9144bbf7570f009742d053f8003520e9
sunnyhyo <sunnyhyo77@gmail.com>
2018-08-08 13:15:37 +0900

    eda

--- bns_setting.R
+++ bns_setting.R
@@ -1,3 +1,6 @@
+library(gapminder)
+library(dplyr)
+
 setwd("C:/Users/HS/Documents/GitHub/champion_data")
 #train data
 activity <- read.csv("./train/train_activity.csv")
@@ -15,3 +18,12 @@ tst_payment <- read.csv("./test/test_payment.csv")
 tst_trade <- read.csv("./test/test_trade.csv")
 
 #EDA
+dim(activity)
+dim(label)
+
+activity<-tbl_df(activity); glimpse(activity); 
+guild<-tbl_df(guild); glimpse(guild)
+label<-tbl_df(label); glimpse(label)
+party<-tbl_df(party); glimpse(party)
+payment<-tbl_df(payment); glimpse(payment)
+trade<-tbl_df(trade); glimpse(trade)
